2401f1ec444f56268e1153e12f70c2cd821fe9f3
Mehran Mousavi <mehran.mousavi@outlook.com>
2023-01-24 07:55:34 +0330

    Check and install Conda Automatically
    Check if Conda is not installed on the system. Download the Miniconda installer and install them. #powershell

--- install_directml_powershell.ps1
+++ install_directml_powershell.ps1
@@ -5,6 +5,25 @@ if ($env_name -eq $null) {
     exit
 }
 
+# Check if Conda is installed on the system
+$conda_installed = $(Get-Command conda -ErrorAction SilentlyContinue)
+
+if ($null -eq $conda_installed) {
+
+    Write-Output "Start downloading and installing miniconda:latest version. This may take a few minutes."
+
+    # Download the Miniconda installer
+    Invoke-WebRequest -Uri https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -OutFile Miniconda3-latest-Windows-x86_64.exe
+
+    # Install Miniconda
+    Start-Process -FilePath .\Miniconda3-latest-Windows-x86_64.exe -ArgumentList "/InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=$env:USERPROFILE\Miniconda3" -Wait
+
+    Write-Output "Miniconda has been installed. Please restart your terminal and run this script again."
+
+    exit
+}
+
+
 # Create new environment
 conda create --name $env_name python=3.10 numpy pandas tensorboard matplotlib tqdm pyyaml -y
 
@@ -15,5 +34,5 @@ conda activate $env_name
 pip install opencv-python
 pip install wget
 pip install torchvision
-conda install pytorch cpuonly -c pytorch
+conda install pytorch cpuonly -c pytorch -y
 pip install torch-directml
